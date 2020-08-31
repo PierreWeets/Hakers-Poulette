b8ac11e3a723d84adab3a0f7ad3aa5fa3c1a7829
PierreWeets <pierre.weets@gmail.com>
2020-08-31 14:16:04 +0200

    1st ver

---
+++ formValidation.php
@@ -0,0 +1,127 @@
+<?php
+//require ('pass.php');
+
+
+//$pass = "Piwi!5830";
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+// Load Composer's autoloader
+require '/home/user/vendor/autoload.php';
+
+//---------------------
+// echo "<pre>";
+// print_r($_POST);// to 
+// echo "</pre>";
+$errors = [];
+$lastName = "";
+$firstName ="";
+$gender = "";
+$email = "";
+
+if(isset($_POST)){
+    $lastName = $_POST['lastName'];
+    $firstName = $_POST['firstName'];
+    $gender = $_POST['gender'];
+    $email = $_POST['email'];
+    $country = $_POST['country'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    // 1. Sanitisation
+    echo "<br>email before Sanitisation : $email";
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    echo "<br>email after Sanitisation : $email";
+
+    // 2. Validation
+    if ( filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+        $errors['email'] = "This address $email is invalid.";
+    } else {
+        echo "<br>email $email is valid.";
+    }
+ 
+    // 3. execution 
+    if (count($errors)> 0){// if no error then
+        echo "<br>There are mistakes!";
+        echo "<br>";
+        print_r($errors);
+        //exit;  stop the execution of the web page
+    } else {
+
+        echo "<br>There are NO mistakes!";
+        echo "<br>Mail sent to the email $email";
+        switch($subject){
+            case "command" : $msg = "We have well received your command : $message \n\nYou must be delivered within the following days.
+                            \nA new mail of confirmation of the delivery of your command will be sent at least 24h before the delivery.\n\nHakers Poulette team.";
+                            break;
+            case "claim"   : $msg = "We have well received your claim : $message. \n\nWe will treat it as soon as possible.\n\nHakers Poulette team.";
+                            break;
+            default : $msg = "We have well received your message : $message\n\nHakers Poulette team.";
+        }
+        
+        // Instantiation and passing `true` enables exceptions
+        $mail = new PHPMailer(true);
+
+        // $Serveur = "smtp.mail.yahoo.com";
+        // $SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        // $port = 465;
+        // $userName = "server.smtp@yahoo.com";
+        // $password ="Password.1234";
+
+
+        $Serveur = "smtp.gmail.com";
+        $SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $port = 465;
+        $userName = "pierre.weets@gmail.com";
+        $password ="Piwi!5830";
+
+        try {
+            //Server settings
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+            $mail->isSMTP();                                            // Send using SMTP
+            $mail->Host       = $Serveur;                    // Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+            $mail->Username   = $userName;                     // SMTP username
+            $mail->Password   = $password;                               // SMTP password
+            $mail->SMTPSecure = $SMTPSecure;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+            $mail->Port       = $port;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+            //Recipients
+            $mail->setFrom($userName, 'Mailer');
+            $mail->addAddress($email , ($firstName.$lastName));     // Add a recipient
+            //$mail->addAddress('ellen@example.com');               // Name is optional
+            // $mail->addReplyTo('info@example.com', 'Information');
+            // $mail->addCC('cc@example.com');
+            // $mail->addBCC('bcc@example.com');
+
+            // Attachments
+            //$mail->addAttachment('/home/user/Learning-Environment/404_not_found/ressources/404NotFound.jpeg');         // Add attachments
+            //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+            // Content
+            //$mail->isHTML(true);                                  // Set email format to HTML
+            $mail->isHTML(true);                                  // Set email as text
+            $mail->Subject = $subject;
+            $mail->Body    = $msg;
+            //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+            $mail->send();
+            echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+    }
+}
+ //come back to the form, passing with method=POST ,the useful data to fill back the form
+echo '<br><br><form method="POST" action="./index.php">
+        <input type="text" name="lastName" value="'.$lastName.'" hidden>
+        <input type="text" name="firstName" value="'.$firstName.'" hidden>
+        <input type="email" name="email" value="'.$email.'" hidden> >
+        <input type="radio" name="gender" value="'.$gender.'" checked hidden>
+        <input type="text" name="country" value="'.$country.'" checked hidden>
+        <input type="text" name="subject" value="'.$subject.'" checked hidden>
+        <input type="text" name="message" value="'.$message.'" checked hidden>
+        <input type="submit" name="submit" value="Return to form"></form>';
+
+?>
---
+++ index.php
@@ -0,0 +1,99 @@
+<?php
+
+echo '<!DOCTYPE html>
+<html lang="en">
+<head><title>Hackers Poulette form</title>
+    <meta charset="utf-8">
+</head>';
+echo '<style>
+
+div {
+    margin-left: auto;
+    margin-right: auto;
+    width: 60%;
+    border-radius: 10px;
+    background-color: #f2f2f2;
+    padding: 30px;
+}
+
+div.center{
+    text-align: center;
+}
+
+input[type=text],select {
+    width: 70%;
+    padding: 5px 10px;
+    margin: 2px 0;
+    display: inline;
+    border: 2px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+  }
+
+ input[type=radio] {
+    display: inline;
+    width: 4%;
+    background-color: #4CAF50;
+    color: white;
+    margin: 4px 0;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+
+ input[type=radio]:hover {
+    border: 0 none;
+    background: blue;
+  }
+
+input[type=submit] {
+    width: 50%;
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+  
+input[type=submit]:hover {
+    background-color: #45a049;
+  }
+  
+
+</style>';
+echo'<body>';
+
+echo '<br><div><form method="POST" action="formValidation.php" >
+
+    <label for="lastName">Lastname</label>
+    <input type="text" id="lastName" name="lastName" required >
+
+    <br><br><label for="firstName">First name</label>
+    <input type="text" id="firstName" name="firstName" required >
+
+    <br><br><label for="Email">Email</label>
+    <input type="email" id="email" name="email" required >
+
+    <br><br><label for="gender">Gender</label>
+    <input type="radio" name="gender" value="M" required >Man
+    <input type="radio" name="gender" value="F" required >Woman
+
+    <br><br><label for="country">Country</label>
+    <input type="text" id="country" name="country" required >
+    
+    <br><br><label for="subject">Subject</label>
+    <select id="subject" name="subject"  >
+        <option value="other" selected>Other</option>
+        <option value="command">Command</option>
+        <option value="claim">Claim</option>
+    </select> 
+    <br><br><label for="message">Message</label>
+    <br><textarea rows="10" cols="100" id="message" name="message" required></textarea>';
+    
+    echo'
+    <br><br><div class="center"><input type="submit" name="submit" value="Send message"></div></form></div>';
+    
+    echo '</body></html>';  
+
+?>
