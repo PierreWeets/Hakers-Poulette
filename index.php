4c1c2edf24a22895d437f4f9c12bae8ef3e7469b
PierreWeets <pierre.weets@gmail.com>
2020-09-03 00:49:10 +0200

    bootstrap & ARIA

--- index.php
+++ index.php
@@ -33,131 +33,106 @@ console_log("test PHP console log");
 <!DOCTYPE html>
 <html lang="en">
 <head><title>Hackers Poulette form</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script> 
+  
     <link rel="stylesheet" href="./css/honeypot.css" type="text/css" charset="utf-8" />
     <link rel="stylesheet" href="./css/hackers.css" type="text/css" charset="utf-8" />
-    <script>
-     console.log("test console");
-    </script> 
 </head>
 
-<style>
-div {
-    margin-left: auto;
-    margin-right: auto;
-    width: 60%;
-    border-radius: 10px;
-    background-color: #f2f2f2;
-    padding: 10px;
-}
-
-div.center{
-    text-align: center;
-}
-
-div.img{
-  max-width:70%; 
-  height:auto
-  padding: 0px;
-  border-radius: 0px;
-  text-align: center;
-  background-color: red;
-}
-
-input[type=text],select {
-    width: 70%;
-    padding: 5px 10px;
-    margin: 2px 0;
-    display: inline;
-    border: 2px solid #ccc;
-    border-radius: 4px;
-    box-sizing: border-box;
-  }
-
- input[type=radio] {
-    display: inline;
-    width: 4%;
-    background-color: #4CAF50;
-    color: white;
-    margin: 4px 0;
-    border-radius: 4px;
-    cursor: pointer;
-  }
-
- input[type=radio]:hover {
-    border: 0 none;
-    background: blue;
-  }
-
-input[type=submit] {
-    width: 50%;
-    background-color: #4CAF50;
-    color: white;
-    padding: 14px 20px;
-    margin: 8px 0;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-  }
-  
-input[type=submit]:hover {
-    background-color: #45a049;
-  }
-</style>
-
 <body>
-    <div class="img"><img src="./img/hackers-poulette-logo.png"></img></div>
-    <br><div><form id='submitForm' method='POST' action='formValidation.php' onsubmit="return toSubmit()" >
+    <div class="container-sm logo"><img role="image" src="./img/hackers-poulette-logo.png"></img></div>
+    <br><div class="container-sm"><form role="form" id='submitForm' method='POST' action='formValidation.php' onsubmit="return toSubmit()" >
 
-    <label for='lastName'>Lastname</label>
-    <input type='text' id='lastName' name='lastName' required value = '<?php echo $lastName ?>'>
+    <label id="lastName-label" for='lastName'>lastName</label>
+    <input aria-required="true" aria-labelledby="lastName-label" role="lastName" type='text' 
+    id='lastName' name='lastName' required value = '<?php echo $lastName ?>' onblur=testName('lastName','lastName-error-label')>
+    <label id="lastName-error-label" class="hidden text-danger">Error : only letters & "-"</label>
 
-    <br><br><label for='firstName'>First name</label>
-    <input type='text' id='firstName' name='firstName' required value = '<?php echo $firstName ?>'>
+    <br><label id="firstName-label" for='firstName'>First name</label>
+    <input aria-required="true" aria-labelledby="firstName-label" role="firstName" type='text' 
+    id='firstName' name='firstName' required value = '<?php echo $firstName ?>' onblur=testName('firstName','firstName-error-label')>
+    <label id="firstName-error-label" class="hidden text-danger">Error : only letters & "-"</label>
 
-    <br><br><label for='Email'>Email</label>
-    <input type='email' id='email' name='email' required value = '<?php echo $email ?>'>
+    <br><label id="email-label" for='Email'>Email</label>
+    <input aria-required="true" aria-labelledby="email-label" role="email" type='email' 
+      id='email' name='email' required value = '<?php echo $email ?>' onblur=testEmail('email','email-error-label') >
+     <label id="email-error-label" class="hidden text-danger">Error : incorrect email address"</label>
 
-    <br><br><label for='gender'>Gender</label>
+    <br><label id="gender-label" for='gender'>Gender</label>
 
-    <input type='radio' name='gender' value='M' required <?php echo ($gender == 'M')? 'checked':'' ?> >Man
-    <input type='radio' name='gender' value='F' required <?php echo ($gender == 'F')? 'checked':'' ?> >Woman
+    <input aria-required="true" aria-labelledby="gender-label" role="genderItem" type='radio' name='gender' value='M' required <?php echo ($gender == 'M')? 'checked':'' ?> >Man
+    <input aria-required="true" aria-labelledby="gender-label" role="genderItem" type='radio' name='gender' value='F' required <?php echo ($gender == 'F')? 'checked':'' ?> >Woman
 
-    <br><br><label for='country'>Country</label>
-        <input type='text' id='country' name='country' required  value = '<?php echo $country ?>'>
+    <br><label id="country-label" for='country'>Country</label>
+    <input aria-required="true" aria-labelledby="country-label" type='text' 
+    id='country' name='country' required  value = '<?php echo $country ?>' onblur=testName('country','country-error-label') >
+    <label id="country-error-label" class="hidden text-danger">Error : only letters & "-"</label>
 
-    <br><br><label for='subject'>Subject</label >
-    <select id='subject' name='subject'>
-    <option value='other' <?php echo ($subject == 'other')? 'selected':'' ?> >Other</option>
-    <option value='command' <?php echo ($subject == 'command')? 'selected':'' ?> >Command</option>
-    <option value='claim' <?php echo ($subject == 'claim')? 'selected':'' ?> >Claim</option>
+    <br><label id="select-label" for='subject'>Subject</label >
+    <select id='subject' aria-labelledby="select-label" aria-required="true" name='subject' >
+    <option aria-current="true" role="subjectItem" value='other' <?php echo ($subject == 'other')? 'selected':'' ?> >Other</option>
+    <option role="subjectItem" value='command' <?php echo ($subject == 'command')? 'selected':'' ?> >Command</option>
+    <option role="subjectItem" value='claim' <?php echo ($subject == 'claim')? 'selected':'' ?> >Claim</option>
     </select>
-    <br><br><label for='message'>Message</label>
-        <br><textarea rows='10' cols='100' id='message' name='message' required ><?php echo $message ?> </textarea>
+    <br><label id="message-label" for='message'>Message</label>
+    <br><div class="container-sm">
+          <textarea aria-required="true" aria-labelledby="message-label" role="message"  
+          id='message' name='message' required ><?php echo $message ?></textarea>
+        </div> 
+    
+    <input id="website" name="website" type="text" value=""/><!-- honey pot-->
 
-    <input id="website" name="website" type="text" value=""/>
-
-    <br><br><div class="center"><input type="submit" name="submit" value="Send message"></div></form></div>
+    <div class="center"><input role="submitButton" type="submit" name="submit" value="Send message"></div></form></div>
 
     <script>
         console.log("document.getElementById('website').value = " + document.getElementById("website").value );
         console.log("document.getElementById('website').value.length = " + document.getElementById("website").value.length );
-        // window.onload = function(){
-        //   document.getElementById("submitForm").addEventListener('submit', event => {
-        //     if (document.getElementById("website").value.length != 0) {
-                  
-        //           //event.preventDefault();
-        //           alert("honey pot filled !!");
-        //           return false;
-        //         } 
-            
-        //   });
-        // }
 
         function toSubmit(){
             if (document.getElementById("website").value.length != 0) {
-                  alert("honey pot filled !!");
+                  alert("honey pot filled !! = a robot has attempted to filled the form. \n\nRespond the following question to continue the form. \n\nAnd resubmit the form.");
+                  if (confirm("Do you want to stop the execution of the form ?")) {//popup window to confirm a decision
+                  } else {//by cancelling , the honey pot is empty and the re-submit of the form will be activated
+                    document.getElementById("website").value="";
+                  }
                   return false;
                 } 
+            
+            if( window.getComputedStyle(document.getElementById("lastName-error-label")).visibility == "visible"){
+              alert("Last name incorrect.");
+              return false;
+            }
+            if( window.getComputedStyle(document.getElementById("firstName-error-label")).visibility == "visible"){
+              alert("First name incorrect.");
+              return false;                
+            }
+            if( window.getComputedStyle(document.getElementById("country-error-label")).visibility == "visible"){
+              alert("Country incorrect.");
+              return false;                
+            }
+        }
+
+        function testName(objectId,labelId) {
+          console.log("testText() : " + document.getElementById(objectId).value);
+          
+        //alert("The input value has changed. The new value is: " + val);
+          if (document.getElementById(objectId).value.match(/[^a-zA-Z-]/)){ //if only letters & 1 "-" between letters then not error
+            document.getElementById(labelId).style.visibility = "visible";
+          }else{
+            document.getElementById(labelId).style.visibility = "hidden";
+          }
+        }
+
+        function testEmail(objectId,labelId){
+          if (! document.getElementById(objectId).value.match(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/)){
+            document.getElementById(labelId).style.visibility = "visible";
+          } else {
+            document.getElementById(labelId).style.visibility = "hidden";
+          }
+
         }
     </script>   
 </body></html>
